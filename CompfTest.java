e531ee346b75f010cd6c30dca2dda9a54ef07d78
JoshKhrustalev <khrustalevjosh@gmail.com>
2019-12-12 15:12:56 +0300

    StackCalcV1

---
+++ CompfTest.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class CompfTest {
+    /*public static void main(String[] args) throws Exception {
+        Scanner in = new Scanner(System.in);
+        Compf c = new Compf();
+        while (true) {
+            System.out.print("Введите формулу -> ");
+            c.compile(in.next().toCharArray());
+        }
+    }*/
+}
